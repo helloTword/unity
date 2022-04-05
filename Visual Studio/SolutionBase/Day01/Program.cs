640a3357feea70e578e2d7c01a656d2a1d059313
WangHuan <785913063@qq.com>
2022-04-05 21:21:28 +0800

    unity、c#学习
    unity游戏开发基础入门学习

---
+++ Visual Studio/SolutionBase/Day01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//字面意思:正在使用xxxx命名空间
+//引入命名空间
+
+
+
+//定义命名空间[类的住址]：对类进行逻辑上的划分，避免重名
+namespace Day01
+{
+    //定义类[做工具]
+    internal class Program
+    {
+        //定义方法[做功能]
+        //程序的入口方法
+        static void Main1(string[] args)
+        {
+
+            //代码格式对齐：Ctrl+A  然后Ctrl+K+F
+            //注释选中的代码：Ctrl+K+C
+            //取消注释 Ctrl+K+U
+
+            //字面意思：控制台.写一行（“内容”）;
+            //现象:在控制台中显示括号内的文本
+            //作用: 将括号中的文本写到控制台z
+
+            Console.WriteLine("你好，世界!");
+
+            // 字面意思:控制台.读一行();
+            //现象：暂停程序(按回车键继续)
+            //作用：将用户在控制台中输入的文本(OK) 读取到程序中来（input）
+            String input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.ReadLine();
+
+            //Console 是类[工具]
+            //WriteLine/ReadLine是方法[动词的功能]
+            //Title 是属性[名词的修辞]
+            //类.方法(); 调用语句[使用某个类中的功能]
+
+
+            String gunName = "AK47";
+            float num1 = 0.1f;
+            double num2 = 2.1;
+
+
+
+
+
+        }
+        static void Main()
+        {
+
+            //float num01 = 3.0f;
+            //float num02 = 2.9f;
+            //float result = num01 - num02;
+            //bool b1 = result == 0.1f;
+            //Console.WriteLine(b1);
+
+            decimal num01 = 3.0m;
+            decimal num02 = 2.9m;
+            decimal result = num01 - num02;
+            bool b1 = result == 0.1m;
+            Console.WriteLine(b1);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Visual Studio/SolutionBase/Day02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String gunName = "AK47";
+            String ammoCapacity = "30";
+            //在字符串中插入变量
+            Console.WriteLine("枪的名字为：" + gunName + ";枪的容量为：" + ammoCapacity);
+            //占位符{位置的编号}   如果编号大于参数列表长度，则异常
+            String gun = String.Format("枪的名字为：{0};枪的容量为：{1}", gunName, ammoCapacity);
+            Console.WriteLine(gun);
+
+
+            // 0:c   {0:c}->¥xx.00  标准数字字符串格式化
+            Console.WriteLine("金额:{0:c}", 10);//货币
+            Console.WriteLine("{0:d2}", 5);  //  :d2 不满足两位，前面添0
+            Console.WriteLine("{0:d2}", 15);
+            Console.WriteLine("{0:f1}", 1.5678);//1.5678  根据指定精度显示
+            Console.WriteLine("{0:p0}", 0.1);//10%  转化为百分数
+
+            //转义 改变字符原始含义 \"  \'
+            Console.WriteLine("我爱\"unity\"!");
+            char c = '\'';
+            Console.WriteLine(c);
+
+            //空字符
+            String a="a131\0131231\0";
+            Console.WriteLine(a);
+        }
+    }
+}
